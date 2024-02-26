c2300ac990eed9eba3bc3b3cc7510a518afec19e
mistichris <codeine500@hotmail.com>
2024-02-26 16:16:51 -0600

    added address model to persist established index page

--- src/main/java/model/Address.java
+++ src/main/java/model/Address.java
@@ -1,19 +1,38 @@
 package model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
  * Larry Paucar - Lpaucar
  * CIS175 -Spring 2024
  * Feb 26, 2024
  */
+@Entity // creates a new table
+@Table(name = "addressbook") // names the table
 public class Address {
-	
+	@Id // Marks as primary key field
+	@GeneratedValue // Auto generates the primary key in the database
+	@Column(name = "ID") // Column Heading Name
+	private int id; // field variable
+	@Column(name="FIRSTNAME")
 	private String firstName;
+	@Column(name="LASTNAME")
 	private String lastName;
+	@Column(name="STREETADDRESS")
 	private String streetAddress;
+	@Column(name="CITY")
 	private String city;
+	@Column(name="STATE")
 	private String state;
+	@Column(name="ZIP")
 	private String zip;
+	@Column(name="PHONE")
 	private String phone;
+	@Column(name="EMAIL")
 	private String email;
 	
 	//Default constructor
