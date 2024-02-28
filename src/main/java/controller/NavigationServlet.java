0f71f002de13d9d2d47db5a32824360c0a9fe1c3
Larry-AP <127695562+Larry-AP@users.noreply.github.com>
2024-02-27 19:31:37 -0600

    Update NavigationServlet.java

--- src/main/java/controller/NavigationServlet.java
+++ src/main/java/controller/NavigationServlet.java
@@ -7,13 +7,19 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import helpers.ContactItemsHelper;
+import model.ContactItems;
+
 /**
  * Servlet implementation class NavigationServlet
  */
-@WebServlet("/NavigationServlet")
+
+//Larry will do
+
+@WebServlet("/navigationServlet")
 public class NavigationServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       //Larry will do
+       
     public NavigationServlet() {
         super();
         // TODO Auto-generated constructor stub
@@ -27,6 +33,34 @@ public class NavigationServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
-	}
+		
+		ContactItemsHelper dao = new ContactItemsHelper();
+		String act = request.getParameter("doThisToItem");
+		
+		String path = "/viewAllItemsServlet";
+		
+		//delete item 
+		if (act.equals("delete")) {
+			try {
+				Integer tempId = Integer.parseInt(request.getParameter("id"));
+				ContactItems itemToDelete = dao.searchForContactItemsById(tempId);
+				dao.deleteItem(itemToDelete);
+				} catch (NumberFormatException e) {
+				System.out.println("Forgot to select an item");
+				}
+		} else if (act.equals("edit")) {
+			try {
+			Integer tempId = Integer.parseInt(request.getParameter("id"));
+			ContactItems itemToEdit = dao.searchForContactItemsById(tempId);
+			request.setAttribute("itemToEdit", itemToEdit);
+			path = "/edit-item.jsp";
+			}catch (NumberFormatException e) {
+				System.out.println("Forgot to select an item");
+			}
+		}else if (act.equals("add")) {
+				 path = "/index.html";
+				 }
+		getServletContext().getRequestDispatcher(path).forward(request, response);
+		}
 
 }
