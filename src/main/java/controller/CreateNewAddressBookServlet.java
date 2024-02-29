90777d2800e1cfd1b6c1849f858067e9abc343fe
mistichris <codeine500@hotmail.com>
2024-02-29 14:00:15 -0600

    testing

--- src/main/java/controller/CreateNewAddressBookServlet.java
+++ src/main/java/controller/CreateNewAddressBookServlet.java
@@ -1,11 +1,21 @@
 package controller;
 
 import java.io.IOException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import helpers.ListDetailsHelper;
+import helpers.ListItemHelper;
+import model.ListDetails;
+import model.ListItem;
+import model.Shopper;
 
 /**
  * Servlet implementation class CreateNewAddressListServlet
@@ -19,7 +29,36 @@ public class CreateNewAddressBookServlet extends HttpServlet {
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		ListItemHelper lih = new ListItemHelper();
+		String listName = request.getParameter("listName");
+		System.out.println("List Name: " + listName);
+		
+		String month = request.getParameter("month");
+		String day = request.getParameter("day");
+		String year = request.getParameter("year");
+		String shopperName = request.getParameter("shopperName");
+				
+		String[] selectedItems = request.getParameterValues("allItemsToAdd");
+		List<ListItem> selectedItemsInList = new ArrayList<ListItem>();
+		// make sure something was selected – otherwise we get a null pointer exception
+		if (selectedItems != null && selectedItems.length > 0) {
+			for (int i = 0; i < selectedItems.length; i++) {
+				System.out.println(selectedItems[i]);
+				ListItem c = lih.searchForItemById(Integer.parseInt(selectedItems[i]));
+				selectedItemsInList.add(c);
+			}
+		}
+		
+		Shopper shopper = new Shopper(shopperName);
+		ListDetails sld = new ListDetails(listName, ld, shopper);
+		sld.setListOfItems(selectedItemsInList);
+		ListDetailsHelper slh = new ListDetailsHelper();
+		slh.insertNewListDetails(sld);
+		
+		System.out.println("Success!");
+		System.out.println(sld.toString());
+		
+		getServletContext().getRequestDispatcher("/viewAllListsServlet").forward(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
